4758bf6c53cafafe0e737cbf8b7db2fca88d87be
Gary Guo <gary.guo@lowrisc.org>
2025-11-19 17:22:35 +0000

    [opentitanlib] make `UartConsole::wait_for` handle line buffering
    Majority of tests work on line-buffered data. Currently, many use
    `.*\r\n` to explicitly handle line ending which is non-ideal. Also, some
    have `[^\r\n]*` trying to consume until end of line, however this won't work
    as we are reading 1 byte at a time from UART, so this will match immediately
    after 1 character.
    
    Make `UartConsole::wait_for` handle the complexity instead. For some tests
    that expects no line endings or expect multiple lines, `wait_for_bytes` can
    be used instead.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/tests/crypto/rsa_kat/src/main.rs
+++ sw/host/tests/crypto/rsa_kat/src/main.rs
@@ -507,7 +507,7 @@ fn run_rsa_testcase(
 fn test_rsa(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let spi = transport.spi("BOOTSTRAP")?;
     let spi_console_device = SpiConsoleDevice::new(&*spi, None)?;
-    let _ = UartConsole::wait_for(&spi_console_device, r"Running [^\r\n]*", opts.timeout)?;
+    let _ = UartConsole::wait_for(&spi_console_device, r"Running ", opts.timeout)?;
 
     let mut test_counter = 0u32;
     let mut failures = vec![];
