cfrantz/opentitan

[signing] Split ECDSA sigverify into start and finish ops
Split ECDSA sigverify into start and finish operations so Ibex and
execute code while OTBN is verifying the signature.

Signed-off-by: Chris Frantz <cfrantz@google.com>

sw/device/silicon_creator/lib/sigverify/ecdsa_p256_verify.c
@@ -118,19 +118,31 @@ static rom_error_t sigverify_encoded_message_check(
   return kErrorSigverifyBadEcdsaSignature;
 }
 
-rom_error_t sigverify_ecdsa_p256_verify(const ecdsa_p256_signature_t *signature,
-                                        const ecdsa_p256_public_key_t *key,
-                                        const hmac_digest_t *act_digest,
+rom_error_t sigverify_ecdsa_p256_start(const ecdsa_p256_signature_t *signature,
+                                       const ecdsa_p256_public_key_t *key,
+                                       const hmac_digest_t *act_digest) {
+  return otbn_boot_sigverify_start(key, signature, act_digest);
+}
+
+rom_error_t sigverify_ecdsa_p256_finish(const ecdsa_p256_signature_t *signature,
                                         uint32_t *flash_exec) {
   ecdsa_p256_signature_t recovered_r;
-  rom_error_t error =
-      otbn_boot_sigverify(key, signature, act_digest, (uint32_t *)&recovered_r);
+  rom_error_t error = otbn_boot_sigverify_finish((uint32_t *)&recovered_r);
   if (launder32(error) != kErrorOk) {
     *flash_exec ^= UINT32_MAX;
     return error;
   }
   HARDENED_CHECK_EQ(error, kErrorOk);
   return sigverify_encoded_message_check(&recovered_r, signature, flash_exec);
+}
+
+rom_error_t sigverify_ecdsa_p256_verify(const ecdsa_p256_signature_t *signature,
+                                        const ecdsa_p256_public_key_t *key,
+                                        const hmac_digest_t *act_digest,
+                                        uint32_t *flash_exec) {
+  HARDENED_RETURN_IF_ERROR(
+      sigverify_ecdsa_p256_start(signature, key, act_digest));
+  return sigverify_ecdsa_p256_finish(signature, flash_exec);
 }
 
 // Extern declarations for the inline functions in the header.

ysekhar/pavona

(cherry picked from commit e1c7d55c45cd1f8315e4226d75cc43770d60fc7b)

sw/device/silicon_creator/lib/sigverify/ecdsa_p256_verify.c
@@ -118,19 +118,31 @@ static rom_error_t sigverify_encoded_message_check(
   return kErrorSigverifyBadEcdsaSignature;
 }
 
-rom_error_t sigverify_ecdsa_p256_verify(const ecdsa_p256_signature_t *signature,
-                                        const ecdsa_p256_public_key_t *key,
-                                        const hmac_digest_t *act_digest,
+rom_error_t sigverify_ecdsa_p256_start(const ecdsa_p256_signature_t *signature,
+                                       const ecdsa_p256_public_key_t *key,
+                                       const hmac_digest_t *act_digest) {
+  return acc_boot_sigverify_start(key, signature, act_digest);
+}
+
+rom_error_t sigverify_ecdsa_p256_finish(const ecdsa_p256_signature_t *signature,
                                         uint32_t *flash_exec) {
   ecdsa_p256_signature_t recovered_r;
-  rom_error_t error =
-      acc_boot_sigverify(key, signature, act_digest, (uint32_t *)&recovered_r);
+  rom_error_t error = acc_boot_sigverify_finish((uint32_t *)&recovered_r);
   if (launder32(error) != kErrorOk) {
     *flash_exec ^= UINT32_MAX;
     return error;
   }
   HARDENED_CHECK_EQ(error, kErrorOk);
   return sigverify_encoded_message_check(&recovered_r, signature, flash_exec);
+}
+
+rom_error_t sigverify_ecdsa_p256_verify(const ecdsa_p256_signature_t *signature,
+                                        const ecdsa_p256_public_key_t *key,
+                                        const hmac_digest_t *act_digest,
+                                        uint32_t *flash_exec) {
+  HARDENED_RETURN_IF_ERROR(
+      sigverify_ecdsa_p256_start(signature, key, act_digest));
+  return sigverify_ecdsa_p256_finish(signature, flash_exec);
 }
 
 // Extern declarations for the inline functions in the header.